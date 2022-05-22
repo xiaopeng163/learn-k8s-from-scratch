0880fa04cfe74fd5446430a59b7cee6512e2cea3
Peng Xiao <xiaoquwl@gmail.com>
2022-05-22 23:10:46 +0200

    kubeadm

---
+++ lab/kubeadm/setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
--- source/k8s-install.rst
+++ source/k8s-install.rst
@@ -1,6 +1,26 @@
 Kubernetes Installation
 ===========================
 
+
+Kubernetes的集群搭建方法有很多种，其中最常见的有：
+
+- Minikube
+- kubeadm
+- Kubernetes on cloud （AWS，Azure，GCP等）
+
+最简单的是第三种，基本就是在云上点一下鼠标就完成了，其次是minikube，一条命令搞定，稍微麻烦一点的是kubeadm。
+
+
+个人比较推荐的是 ``kubeadm``, 原因是在CKA的考试中，kubeadm相关的内容是必考内容之一，包括：
+
+- Use Kubeadm to install a basic cluster
+- Perform a version upgrade on a Kubernetes cluster using Kubeadm
+
+
+
+主要内容
+~~~~~~~~~~
+
 .. toctree::
     :maxdepth: 1
 
