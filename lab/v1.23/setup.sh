75eab15dbf730870ade3be3c0581bea0d48afebf
Peng Xiao <xiaoquwl@gmail.com>
2022-07-18 22:44:05 +0200

    update

---
+++ lab/v1.23/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+echo "[TASK 5] Install containerd runtime"
+apt update -qq >/dev/null 2>&1
+apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+apt install -qq -y nfs-common >/dev/null 2>&1
+mkdir /etc/containerd
+containerd config default >/etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null 2>&1
+
+echo "[TASK 6] Add apt repo for kubernetes"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+
+echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt install -qq -y kubeadm=1.23.8-00 kubelet=1.23.8-00 kubectl=1.23.8-00 >/dev/null 2>&1
+
+#https://blog.sighup.io/how-to-run-kubernetes-without-docker/
