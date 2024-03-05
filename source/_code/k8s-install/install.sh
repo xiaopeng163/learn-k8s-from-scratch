de2f1f917b699dbcbb57ebdd267508ce900356ac
Peng Xiao <xiaoquwl@gmail.com>
2024-03-05 16:37:10 +0100

    update

---
+++ source/_code/k8s-install/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf<<EOF
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+echo "[TASK 5] Install containerd runtime"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt -qq update >/dev/null 2>&1
+apt install -qq -y containerd.io >/dev/null 2>&1
+containerd config default >/etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null 2>&1
+
+echo "[TASK 6] Add apt repo for kubernetes"
+apt-get install -y apt-transport-https ca-certificates curl gpg >/dev/null 2>&1
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+
+echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt -qq update >/dev/null 2>&1
+apt install -qq -y kubeadm=1.29.2-1.1 kubelet=1.29.2-1.1 kubectl=1.29.2-1.1  >/dev/null 2>&1
