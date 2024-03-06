7b870fc0db08c9239a634ffa85be181386b212b4
Peng Xiao <xiaoquwl@gmail.com>
2024-03-06 12:05:09 +0100

    update

--- source/_code/k8s-install/install.sh
+++ source/_code/k8s-install/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# swap already support https://kubernetes.io/blog/2023/08/24/swap-linux-beta/ if you want to enalbe swap, please check the link
 echo "[TASK 1] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-echo "[TASK 2] Stop and Disable firewall"
+echo "[TASK 2] install some tools"
+apt install -qq -y vim jq iputils-ping net-tools >/dev/null 2>&1
+
+echo "[TASK 3] Stop and Disable firewall"
 systemctl disable --now ufw >/dev/null 2>&1
 
-echo "[TASK 3] Enable and Load Kernel modules"
+echo "[TASK 4] Enable and Load Kernel modules"
 cat >>/etc/modules-load.d/containerd.conf<<EOF
 overlay
 br_netfilter
@@ -15,7 +19,7 @@ EOF
 modprobe overlay
 modprobe br_netfilter
 
-echo "[TASK 4] Add Kernel settings"
+echo "[TASK 5] Add Kernel settings"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables  = 1
@@ -23,7 +27,7 @@ net.ipv4.ip_forward                 = 1
 EOF
 sysctl --system >/dev/null 2>&1
 
-echo "[TASK 5] Install containerd runtime"
+echo "[TASK 6] Install containerd runtime"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
@@ -35,7 +39,7 @@ sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.to
 systemctl restart containerd
 systemctl enable containerd >/dev/null 2>&1
 
-echo "[TASK 6] Add apt repo for kubernetes"
+echo "[TASK 7] Add apt repo for kubernetes"
 apt-get install -y apt-transport-https ca-certificates curl gpg >/dev/null 2>&1
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
